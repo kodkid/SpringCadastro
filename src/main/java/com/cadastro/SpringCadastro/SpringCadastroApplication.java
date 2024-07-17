10e201267b3e17b426a65dc543e47e39a0084300
kodkid <math_moss@hotmail.com>
2024-07-16 22:16:32 -0400

    Projeto CadastroSpring

---
+++ src/main/java/com/cadastro/SpringCadastro/SpringCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.SpringCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCadastroApplication.class, args);
+	}
+
+}
